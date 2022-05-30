af08c31805bb8365824cfb8fc425e4f144872c48
Pedro Filipe Justo Nunes <pfjn@PfJnAir.local>
2022-05-30 17:43:55 +0100

    Re-Do Initial Setup

--- Livros/app/src/main/java/pt/pfjn/livros/TabelaBDCategorias.kt
+++
@@ -1,9 +0,0 @@
-package pt.pfjn.livros
-
-import android.database.sqlite.SQLiteDatabase
-
-class TabelaBDCategorias(val db: SQLiteDatabase) {
-    fun criaTabela() {
-        db.execSQL("CREATE TABLE CATEGORIAS ")
-    }
-}
