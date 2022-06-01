2d4fddba981d5b5ac2b50b11367bfe3056769ccc
pfjndev <88943477+pfjndev@users.noreply.github.com>
2022-06-01 15:52:25 +0100

    Created AdministrationDBTable class. Administration database table class.

---
+++ app/src/main/java/com/pfjn/pnvapp/AdministrationDBTable.kt
@@ -0,0 +1,10 @@
+package com.pfjn.pnvapp
+
+import android.database.sqlite.SQLiteDatabase
+
+class AdministrationDBTable(db: SQLiteDatabase) {
+    fun init() {
+        TODO("Not yet implemented")
+    }
+
+}
